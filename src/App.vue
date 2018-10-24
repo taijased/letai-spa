ea9bf13a52313cb5997e5e0bd6a1ed1431dd41cb
Максим Спиридонов <mvspiridonov@arq.su>
2018-10-24 23:07:13 +0300

    Create project

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -25,5 +21,9 @@
       color: #42b983;
     }
   }
+}
+* {
+	margin: 0;
+	padding: 0;
 }
 </style>
---
+++ src/views/Index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="wrapper">
+        <header>
+            <img class="logo" src="../assets/logo.svg" alt="tattelecom">
+            <div class="internationalization noselect" @click="setLocalization()">{{$t("internationalization.title")}}</div>
+        </header>
+        <main>
+            <aside class="aside-left">
+                <div>left</div>
+            </aside>
+            <aside class="aside-right">
+                <div>
+                    right
+                    <router-view/>
+                </div>
+            </aside>
+        </main>
+        <footer>
+            <div>© ПАО «Таттелеком» 2018</div>
+            <div>powered by <span>tim agency</span></div>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+ methods: {
+    setLocalization() {
+      this.$i18n.locale = this.$i18n.locale === "ru" ? "en" : "ru";
+    }
+  }
+}
+</script>
+
+<style lang="less">
+@import url('../style/index.less');
+
+</style>
